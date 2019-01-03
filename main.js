1b7b4c565d13592b48c8a489d8aee0d3f209a385
tking <b250146196@gmail.com>
2019-01-03 19:50:08 +0800

    debug

--- main.js
+++ main.js
@@ -25,18 +25,39 @@ app.get('/wx' , (req , res) => {
 })
 
 app.post('/wx' , (req , respond) => {
-    const xml = req.body
-    console.log('from post body' , xml)
-//     const xml = `<xml><ToUserName><![CDATA[gh_3d9c0b7d4d2f]]></ToUserName><FromUserName><![CDATA[ofRPM0swXBeqFKC-WW
-// OMyN2_Dxak]]></FromUserName><CreateTime>1546494018</CreateTime><MsgType><![CDATA[text]]></MsgType><Content>
-// <![CDATA[sdfsdf]]></Content><MsgId>6642141231231886372</MsgId></xml>`
-    parseXML(xml).then(res=>{
-        console.log(res)
-        res = resText(res.xml)
-        respond.send(res)
-    }).catch(err=>{
-        respond.send(`error : ${err}`)
-    })
+
+    var jsonString = '';
+
+    req.on('data', function (data) {
+        jsonString += data;
+    });
+
+    req.on('end', function () {
+        const xml = JSON.parse(jsonString)
+        console.log('xml res : ' , xml);
+
+        parseXML(xml).then(res=>{
+            console.log(res)
+            res = resText(res.xml)
+            respond.send(res)
+        }).catch(err=>{
+            respond.send(`error : ${err}`)
+        })
+    });
+
+
+//     const xml = req.body
+//     console.log('from post body' , xml)
+// //     const xml = `<xml><ToUserName><![CDATA[gh_3d9c0b7d4d2f]]></ToUserName><FromUserName><![CDATA[ofRPM0swXBeqFKC-WW
+// // OMyN2_Dxak]]></FromUserName><CreateTime>1546494018</CreateTime><MsgType><![CDATA[text]]></MsgType><Content>
+// // <![CDATA[sdfsdf]]></Content><MsgId>6642141231231886372</MsgId></xml>`
+//     parseXML(xml).then(res=>{
+//         console.log(res)
+//         res = resText(res.xml)
+//         respond.send(res)
+//     }).catch(err=>{
+//         respond.send(`error : ${err}`)
+//     })
 })
 
 // app.all(function (req , res , next) {
