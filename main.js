64d37545fa31374f2445f527000919f202f1d790
tking <b250146196@gmail.com>
2019-01-04 20:36:41 +0800

    xml

--- main.js
+++ main.js
@@ -1,3 +1,4 @@
+import './index'
 import express from 'express'
 import bodyParser from 'body-parser'
 import registerRouter from './controller'
--- run.sh
+++ run.sh
@@ -1,6 +1,6 @@
 git pull origin
-docker stop $(docker ps -aq)
-docker rm $(docker ps -aq)
+docker stop server
+docker rm server
 docker image rm node/wx
 docker build -t node/wx .
 docker run --name server -d -p 80:80 node/wx
